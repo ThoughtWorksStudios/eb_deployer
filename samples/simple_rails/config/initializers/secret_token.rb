54c2fee2445d715bba57ff7cfd4da8d761d56879
wpc <alex.hal9000@gmail.com>
2013-09-28 18:41:29 -0700

    delete old sample projects

--- samples/simple_rails/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-SimpleRails::Application.config.secret_token = '3ce95a177716930d4b286d0fc5da2eb638b50d7890a7fa610228ae7744137d94fb679c435e4cae7a565ea6be38b0fafb153e39028cf293525bb1c5c6c17067c8'
